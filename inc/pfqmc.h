4686e00437890194596afbd3368fd843dbbd4e26
zhouquanwan <zhouquanwan@zhouquanwandeMacBook-Pro.local>
2024-04-16 20:56:31 +0800

    rewrite udt api, correct pfqmc init sweep, test to do

--- inc/pfqmc.h
+++ inc/pfqmc.h
@@ -1,83 +1,122 @@
 #ifndef PFQMC_H
 #define PFQMC_H
-
 #include "honeycomb.h"
 #include "qr_udt.h"
 
-class PfQMC {
-   public:
+class PfQMC
+{
+public:
     int stb;
     int nDim;
     MatType g;
-    std::vector<Operator *> *op_array;
+    std::vector<Operator *> op_array;
     int op_length;
     std::vector<bool> need_stabilization;
     int checkpoints;
-    std::vector<UDT*> udtR;
-    UDT* Al;
+    std::vector<UDT> udtL;
+    std::vector<UDT> udtR;
 
     PfQMC(Honeycomb_tV *walker, int _stb = 10);
 
-    void rightInit() {
-        int id = 0;
-        MatType Aseg, Atmp;
-        for (int i=0; i<checkpoints; i++) {
-            Aseg = MatType::Identity(nDim, nDim);
-            for (int j=0; j<stb; j++) {
-                op_array->at(id)->left_multiply(Aseg, Atmp);
-                std::swap(Aseg, Atmp);
-                id++; 
-                if (id >= op_length) break;
+    void rightInit()
+    {
+        MatType tmp = MatType::Identity(nDim, nDim);
+        MatType Aseg = MatType::Identity(nDim, nDim);
+        int curSeg = 0;
+        for (int l = 0; l < op_length; l++)
+        {
+            op_array[l]->left_multiply(Aseg, tmp);
+            std::swap(Aseg, tmp);
+            // %op_length is important, cannot be remove. or else the last segment will not be calculated
+            if (need_stabilization[(l + 1) % op_length])
+            {
+                if (curSeg == 0)
+                {
+                    udtR[curSeg] = UDT(Aseg); // TODO: performance check
+                }
+                else
+                {
+                    udtR[curSeg] = Aseg * udtR[curSeg - 1];
+                }
+                Aseg = MatType::Identity(nDim, nDim);
+                curSeg++;
             }
-            udtR[i] = new UDT(Aseg, nDim);
         }
-        
-        // initialize green function g
-        Al = new UDT(*udtR[0]);
-        for (int i=1; i<checkpoints; i++) {
-            udtR[i]->factorizedMultUpdate(*Al, nDim);
+        udtR[checkpoints - 1].onePlusInv(g);
+    }
+
+    void leftInit()
+    {
+        MatType tmp = MatType::Identity(nDim, nDim);
+        MatType Aseg = MatType::Identity(nDim, nDim);
+        int curSeg = checkpoints - 1;
+        for (int l = op_length - 1; l > -1; l--)
+        {
+            op_array[l]->right_multiply(Aseg, tmp);
+            std::swap(Aseg, tmp);
+            if (need_stabilization[l])
+            {
+                Aseg.adjointInPlace();
+                if (curSeg == (checkpoints - 1))
+                {
+                    udtL[curSeg] = UDT(Aseg); // TODO: performance check
+                }
+                else
+                {
+                    udtL[curSeg] = Aseg * udtL[curSeg + 1];
+                }
+                Aseg = MatType::Identity(nDim, nDim);
+                curSeg--;
+            }
         }
-        Al->onePlusInv(nDim, g);
+        udtL[0].onePlusInv(g);
+        g.adjointInPlace();
     }
 
     // after each sweep
     // the greens function g
     // is automatically updated
-    void sweep();
+    void rightSweep();
+    void leftSweep();
 
-    DataType getSign() {
+    DataType getSign()
+    {
         const MatType identity = MatType::Identity(nDim, nDim);
         const DataType extraSign = ((nDim / 2) % 2 == 0) ? 1.0 : -1.0;
         UDT A(nDim);
-        op_array->at(0)->stabilizedLeftMultiply(A);
+        op_array[0]->stabilizedLeftMultiply(A);
         MatType gNext, gCur;
         DataType signCur, signNext, signPfaf;
-        signCur = op_array->at(0)->getSign();
-        A.onePlusInv(nDim, gCur); gCur -= identity;
-        for (int i=1; i<op_length; i++) {
-            op_array->at(i)->getGreensMat(gNext);
-            signNext = op_array->at(i)->getSign();
+        signCur = op_array[0]->getSign();
+        A.onePlusInv(gCur);
+        gCur -= identity;
+        for (int i = 1; i < op_length; i++)
+        {
+            op_array[i]->getGreensMat(gNext);
+            signNext = op_array[i]->getSign();
             // std::cout << gNext << "==== gnext ====\n \n";
             // std::cout << gCur << "==== gcur ====\n \n";
             signPfaf = pfaffianForSignOfProduct(gNext, gCur);
             signCur = (signCur * signNext * signPfaf * extraSign);
-            
+
             // std::cout << signCur << " sign cur\n";
-            if (i == op_length-1) break;
-            op_array->at(i)->stabilizedLeftMultiply(A);
-            A.onePlusInv(nDim, gCur); gCur -= identity;
+            if (i == op_length - 1)
+                break;
+            op_array[i]->stabilizedLeftMultiply(A);
+            A.onePlusInv(gCur);
+            gCur -= identity;
         }
         return signCur;
     }
-
-    ~PfQMC() {
-        for (int i=0; i<udtR.size(); i++) {
-            delete udtR[i];
-        }
-        delete Al;
-    }
+    
+    // ~PfQMC()
+    // {
+        // for (int i = 0; i < udtR.size(); i++)
+        // {
+        //     delete udtR[i];
+        // }
+        // delete Al;
+    // }
 };
-
-
 
 #endif
--- inc/qr_udt.h
+++ inc/qr_udt.h
@@ -6,131 +6,248 @@
 class UDT
 {
 public:
+    int nDim;
     MatType U;
     dVecType D;
     MatType T;
-
-    UDT(int nDim) {
+    UDT() = default;
+    UDT(int nDim)
+    {
+        this->nDim = nDim;
         U = MatType::Identity(nDim, nDim);
         D = dVecType::Ones(nDim);
         T = MatType::Identity(nDim, nDim);
     }
 
-    UDT(const MatType& _U, const dVecType& _D, const MatType& _T) {
+    UDT(const MatType &_U, const dVecType &_D, const MatType &_T)
+    {
+        this->nDim = _U.rows();
         U = _U;
         D = _D;
         T = _T;
     }
 
-    // UDT& operator=(const UDT& other) {
-    //     UDT F(other.U, other.D, other.T);
-    //     return F;
-    // }
+    UDT &operator=(const UDT &other)
+    {
+        nDim = other.nDim;
+        U = other.U;
+        D = other.D;
+        T = other.T;
+        return *this;
+    }
+
+    UDT(const UDT &other)
+    {
+        *this = other;
+    }
+
+    UDT &operator=(UDT &&other)
+    {
+        if (this != &other)
+        {
+            nDim = other.nDim;
+            U = std::move(other.U);
+            D = std::move(other.D);
+            T = std::move(other.T);
+        }
+    }
+
+    UDT(UDT &&other)
+    {
+        *this = std::move(other);
+    }
 
     // use qr to get UDT decomposition
-    UDT(MatType& A, const int nDim) {
+    UDT(MatType &A)
+    {
+        nDim = A.rows();
         T = MatType::Zero(nDim, nDim);
         D = dVecType(nDim);
         int jpvt[nDim];
         DataType tau[nDim];
-        for(int j=0; j<nDim; j++) {
+        for (int j = 0; j < nDim; j++)
+        {
             jpvt[j] = 0;
         }
 
         LAPACKE_zgeqp3(LAPACK_COL_MAJOR, nDim, nDim, A.data(), nDim, jpvt, tau);
 
         double alpha;
-        for (int i=0; i<nDim; i++) {
+        for (int i = 0; i < nDim; i++)
+        {
             D(i) = std::abs(A(i, i).real()); // A(i, i)'s are all real
             alpha = 1.0 / D(i);
-            for (int j = i; j < nDim; j++) {
-			    A(i, j) = A(i, j) * alpha;
-		    }
+            for (int j = i; j < nDim; j++)
+            {
+                A(i, j) = A(i, j) * alpha;
+            }
         }
-        
+
         int j;
-        for (int i=0; i<nDim; i++) {
+        for (int i = 0; i < nDim; i++)
+        {
             j = jpvt[i] - 1;
             T(i, j) = 1;
         }
 
-        cblas_ztrmm(CblasColMajor, CblasLeft, CblasUpper,   CblasNoTrans, CblasNonUnit, nDim, nDim, &one, A.data(), nDim, T.data(), nDim);
+        cblas_ztrmm(CblasColMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, nDim, nDim, &one, A.data(), nDim, T.data(), nDim);
 
         LAPACKE_zungqr(LAPACK_COL_MAJOR, nDim, nDim, nDim, A.data(), nDim, tau);
 
         U = A;
     }
 
-    // F = (*this) * F
-    inline void factorizedMultUpdate(UDT& F, int nDim) {
-        MatType mat = T * F.U;
-        mat = D.asDiagonal() * mat;
-        mat = mat * F.D.asDiagonal();
-        UDT tmp(mat, nDim);
-        F.U = U * tmp.U;
-        F.T = tmp.T * F.T;
-        F.D = tmp.D;
-    }
+    // // F = (*this) * F
+    // inline void factorizedMultUpdate(UDT &F)
+    // {
+    //     MatType mat = T * F.U;
+    //     mat = D.asDiagonal() * mat;
+    //     mat = mat * F.D.asDiagonal();
+    //     UDT tmp(mat);
+    //     F.U = U * tmp.U;
+    //     F.T = tmp.T * F.T;
+    //     F.D = tmp.D;
+    // }
 
-    // (*this) = B * (*this)
-    inline void bMultUpdate(const MatType& B, int nDim) {
-        // MatType r = ((B * U) * D.asDiagonal()) * T;
-        MatType tmp = B * U;
-        MatType tmp2 = tmp * D.asDiagonal();
-        UDT F(tmp2, nDim);
-        U = F.U;
-        D = F.D;
-        // std::cout << " r - UDTT " << (r - U * D.asDiagonal() * F.T * T).squaredNorm() << "\n";
-        tmp = (F.T) * T;
-        T = tmp;
-    }
+    // // (*this) = B * (*this)
+    // inline void bMultUpdate(const MatType &B)
+    // {
+    //     // MatType r = ((B * U) * D.asDiagonal()) * T;
+    //     MatType tmp = B * U;
+    //     MatType tmp2 = tmp * D.asDiagonal();
+    //     UDT F(tmp2);
+    //     U = F.U;
+    //     D = F.D;
+    //     // std::cout << " r - UDTT " << (r - U * D.asDiagonal() * F.T * T).squaredNorm() << "\n";
+    //     tmp = (F.T) * T;
+    //     T = tmp;
+    // }
+
+    // // g = 2 * (1 + UDT)^{-1}
+    // inline void onePlusInv(MatType &g) const
+    // {
+    //     MatType Xinv = T;
+    //     MatType tmp1, tmp2;
+    //     // std::cout << Tn << "\n === \n";
+    //     int ipiv[nDim];
+    //     LAPACKE_zgetrf(LAPACK_COL_MAJOR, nDim, nDim, Xinv.data(), nDim, ipiv);
+    //     // std::cout << "ipiv= " << ipiv << "\n";
+    //     // std::cout << Tn << "\n";
+    //     LAPACKE_zgetri(LAPACK_COL_MAJOR, nDim, Xinv.data(), nDim, ipiv);
+
+    //     // std::cout << "Xinv * X - I = " << (Xinv*T - MatType::Identity(nDim, nDim)).squaredNorm() << "\n";
+
+    //     dVecType Dpinv(nDim);
+    //     dVecType Dm(nDim);
+    //     for (int i = 0; i < nDim; i++)
+    //     {
+    //         Dpinv(i) = 1.0 / std::max(D(i), 1.0);
+    //         Dm(i) = std::min(D(i), 1.0);
+    //     }
+
+    //     tmp1 = Xinv * Dpinv.asDiagonal();
+    //     tmp2 = U * Dm.asDiagonal();
+    //     tmp1 = tmp1 + tmp2;
+
+    //     UDT f = UDT(tmp1);
+    //     LAPACKE_zgetrf(LAPACK_COL_MAJOR, nDim, nDim, f.T.data(), nDim, ipiv);
+    //     LAPACKE_zgetri(LAPACK_COL_MAJOR, nDim, f.T.data(), nDim, ipiv);
+
+    //     // std::cout << "Uinv * U - I = " << ((f.U)*(f.U.adjoint()) - MatType::Identity(nDim, nDim)).squaredNorm() << "\n";
+
+    //     // MatType identity = MatType::Identity(nDim, nDim);
+    //     // g = Xinv * Dpinv.asDiagonal() * f.T * (f.U * f.D.asDiagonal()).inverse();
+    //     // return;
+
+    //     tmp1 = (f.T) * (f.D.cwiseInverse()).asDiagonal();
+    //     tmp2 = tmp1 * f.U.adjoint();
+    //     tmp1 = Dpinv.asDiagonal() * tmp2;
+
+    //     f = UDT(tmp1);
+
+    //     f.D *= 2.0;
+
+    //     tmp2 = Xinv * f.U;
+    //     tmp1 = tmp2 * f.D.asDiagonal();
+    //     g = tmp1 * f.T;
+    // }
+
 
     // g = 2 * (1 + UDT)^{-1}
-    inline void onePlusInv(int nDim, MatType& g) const {
-        MatType Xinv = T;
-        MatType tmp1, tmp2;
-        // std::cout << Tn << "\n === \n";
-        int ipiv[nDim];
-        LAPACKE_zgetrf(LAPACK_COL_MAJOR, nDim, nDim, Xinv.data(), nDim, ipiv);
-        // std::cout << "ipiv= " << ipiv << "\n";
-        // std::cout << Tn << "\n";
-        LAPACKE_zgetri(LAPACK_COL_MAJOR, nDim, Xinv.data(), nDim, ipiv);
-
-        // std::cout << "Xinv * X - I = " << (Xinv*T - MatType::Identity(nDim, nDim)).squaredNorm() << "\n";
-
+    inline void onePlusInv(MatType &g) const
+    {
+        MatType Xinv = T.inverse();
         dVecType Dpinv(nDim);
         dVecType Dm(nDim);
-        for(int i=0; i<nDim; i++) {
+        for (int i = 0; i < nDim; i++)
+        {
             Dpinv(i) = 1.0 / std::max(D(i), 1.0);
             Dm(i) = std::min(D(i), 1.0);
         }
-
-        tmp1 = Xinv * Dpinv.asDiagonal();
-        tmp2 = U * Dm.asDiagonal();
-        tmp1 = tmp1 + tmp2;
-
-        UDT f = UDT(tmp1, nDim);
-        LAPACKE_zgetrf(LAPACK_COL_MAJOR, nDim, nDim, f.T.data(), nDim, ipiv);
-        LAPACKE_zgetri(LAPACK_COL_MAJOR, nDim, f.T.data(), nDim, ipiv);
-
-        // std::cout << "Uinv * U - I = " << ((f.U)*(f.U.adjoint()) - MatType::Identity(nDim, nDim)).squaredNorm() << "\n";
-
-        // MatType identity = MatType::Identity(nDim, nDim);
-        // g = Xinv * Dpinv.asDiagonal() * f.T * (f.U * f.D.asDiagonal()).inverse();
-        // return;
-
-        tmp1 = (f.T) * (f.D.cwiseInverse()).asDiagonal();
-        tmp2 = tmp1 * f.U.adjoint();
-        tmp1 = Dpinv.asDiagonal() * tmp2;
-
-        f = UDT(tmp1, nDim);
-
-        f.D *= 2.0;
-
-        tmp2 = Xinv * f.U;
-        tmp1 = tmp2 * f.D.asDiagonal();
-        g = tmp1 * f.T;
+        MatType tmp1 = Xinv * Dpinv.asDiagonal();
+        MatType tmp2 = (tmp1 + U * Dm.asDiagonal()).inverse();
+        g = 2.0 * tmp1 * tmp2;
     }
 };
 
+inline UDT operator*(const UDT &udtL, const UDT &udtR)
+{
+    MatType mat = udtL.T * udtR.U;
+    mat = udtL.D.asDiagonal() * mat;
+    mat = mat * udtR.D.asDiagonal();
+    UDT tmp(mat);
+    tmp.U = udtL.U * tmp.U;
+    tmp.T = tmp.T * udtR.T;
+    return tmp;
+}
+
+// TO DO: lazy evaluation, check udta = B * udta
+inline UDT operator*(const MatType &B, const UDT &udtR)
+{
+    MatType mat = B * udtR.U;
+    mat = mat * udtR.D.asDiagonal();
+    UDT tmp(mat);
+    tmp.T = tmp.T * udtR.T;
+    return tmp;
+}
+
+// return (1+udtR@(udtL).adjoint)^{-1}
+inline MatType onePlusInv(UDT &udtL, UDT &udtR)
+{
+    int n = udtR.U.cols();
+    MatType tem1 = udtR.U.adjoint() * udtL.U;
+    MatType tem2 = udtR.T * udtL.T.adjoint();
+    auto DrPinv = dVecType(n);
+    auto DrM = dVecType(n);
+    auto DlPinv = dVecType(n);
+    auto DlM = dVecType(n);
+    for (int i = 0; i < n; i++)
+    {
+        if (std::abs(udtR.D(i)) > 1.0)
+        {
+            DrPinv(i) = 1.0 / udtR.D(i);
+            DrM(i) = 1.0;
+        }
+        else
+        {
+            DrPinv(i) = 1.0;
+            DrM(i) = udtR.D(i);
+        }
+        if (std::abs(udtL.D(i)) > 1.0)
+        {
+            DlPinv(i) = 1.0 / udtL.D(i);
+            DlM(i) = 1.0;
+        }
+        else
+        {
+            DlPinv(i) = 1.0;
+            DlM(i) = udtL.D(i);
+        }
+    }
+
+    tem2 = DrPinv.asDiagonal() * tem1 * DlPinv.asDiagonal() + DrM.asDiagonal() * tem2 * DlM.asDiagonal();
+    tem1 = DlPinv.asDiagonal() * tem2.inverse() * DrPinv.asDiagonal();
+
+    return 2. * udtL.U * tem1 * (udtR.U.adjoint());
+}
 #endif
